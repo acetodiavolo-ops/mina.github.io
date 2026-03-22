282bcd2404da2567adbe9e22e4fb1da9c00d5814
acetodiavolo-ops <acetodiavolo@gmail.com>
2026-03-22 17:06:07 +0100

    Your message here

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var CACHE = 'iglisi-v12';
+var CACHE = 'iglisi-v13';
 
 var PRECACHE = [
   '/en/',
@@ -15,7 +15,7 @@ var PRECACHE = [
   '/it/blog/segni-revisione-orologio.html',
   '/sq/blog/guide-bateria-ores.html',
   '/sq/blog/shenjat-riparimit-ores.html',
-  '/shared.css?v=19',
+  '/shared.css?v=20',
   '/shared.js?v=5',
   '/webfonts/inter.woff2?v=2',
   '/webfonts/cormorant-garamond.woff2?v=2',
